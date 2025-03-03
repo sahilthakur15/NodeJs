ac3a51be0e8320abe68b219fbed4f9a67a0348fd
sahilthakur15 <sahilthakurwins@gmail.com>
2025-03-03 19:01:48 +0530

    Http methods

---
+++ Express/app.js
@@ -0,0 +1,5 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const express = require("express");
+
--- Server/index.js
+++ Server/index.js
@@ -4,14 +4,45 @@ const url = require("url");
 
 
 
-// creating server
-const myServer= http.createServer((req, res) =>{
+// ...............creating server
+
+// const myServer= http.createServer((req, res) =>{
+//     if(req.url==="/favicon.ico") return res.end();
+//     const log = `${Date.now()}: ${req.url} New Req Recived\n`;
+//     const myUrl= url.parse(req.url, true);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt", log, (err,data)=>{
+//         // res.end("Hello From Server");  
+//         switch(myUrl.pathname){
+//             case "/":
+//                 res.end("HomePage");
+//                 break;
+//             case "/about":
+//                 const username= myUrl.query.myname;
+//                 res.end(`Hi, ${username}`);
+//                 break;
+//             case "/search":
+//                 const search = myUrl.query.search_query;
+//                 res.end("Here are your results for "+ search);
+//                 break;
+//             default:
+//                 res.end("404 Not Found");
+//         }
+
+//     });
+
+//     // console.log("New Req Rec.");  
+// });
+
+
+// ...........Http Methods
+
+ const myServer=http.createServer((req, res)=>{
     if(req.url==="/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.url} New Req Recived\n`;
-    const myUrl= url.parse(req.url, true);
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recived\n`;
+    const myUrl = url.parse(req.url, true);
     console.log(myUrl);
-    fs.appendFile("log.txt", log, (err,data)=>{
-        // res.end("Hello From Server");  
+    fs.appendFile("log.txt", log, (err, data)=>{
         switch(myUrl.pathname){
             case "/":
                 res.end("HomePage");
@@ -21,17 +52,14 @@ const myServer= http.createServer((req, res) =>{
                 res.end(`Hi, ${username}`);
                 break;
             case "/search":
-                const search = myUrl.query.search_query;
+                const search = myUrl.query.search_query; 
                 res.end("Here are your results for "+ search);
                 break;
             default:
-                res.end("404 Not Found");
+                res.end("404 Not found");
         }
-
     });
+ });
 
-    // console.log("New Req Rec.");  
-
-});
 myServer.listen(8001, () => console.log("Server Started"));
 
